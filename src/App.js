405d688fb1e6d8efee513fd56c5820b48695b8b0
Ravi Bossema <ravi.bossema@gmail.com>
2022-02-19 01:20:03 +0100

    Additional theme added (better I think)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import { About, Navbar, Projects } from './components';
-import { theme } from './theme';
+import { DSTheme, BardTheme } from './theme';
 
 export default function App() {
 	return (
-		<ThemeProvider theme={theme}>
+		<ThemeProvider theme={BardTheme}>
 			<Navbar />
 			<About />
 			<Projects />
--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,8 @@
 import { createTheme } from '@mui/material';
 
-export const theme = createTheme({
+export const DSTheme = createTheme({
 	palette: {
-		type: 'dark',
+		mode: 'dark',
 		primary: {
 			main: '#d3f9fd',
 			contrastText: '#032324',
@@ -20,6 +20,44 @@ export const theme = createTheme({
 			disabled: 'rgba(211,249,253,0.5)',
 			hint: 'rgba(211,249,253,0.5)',
 		},
+		divider: 'rgba(147,174,177,0.2)',
+	},
+	components: {
+		MuiAppBar: {
+			styleOverrides: {
+				root: {
+					backgroundColor: 'transparent',
+					backdropFilter: 'blur(10px)',
+				},
+			},
+		},
+		MuiTooltip: {
+			arrow: true,
+		},
+	},
+});
+
+export const BardTheme = createTheme({
+	palette: {
+		mode: 'dark',
+		primary: {
+			main: '#b8e7e7',
+			contrastText: '#090b1c',
+		},
+		secondary: {
+			main: '#124d53',
+		},
+		background: {
+			default: '#090b1c',
+			paper: '#16264a',
+		},
+		text: {
+			primary: '#b8e7e7',
+			secondary: 'rgba(184,231,231,0.7)',
+			disabled: 'rgba(184,231,231,0.5)',
+			hint: 'rgba(184,231,231,0.5)',
+		},
+		divider: 'rgba(184,231,231,0.12)',
 	},
 	components: {
 		MuiAppBar: {
